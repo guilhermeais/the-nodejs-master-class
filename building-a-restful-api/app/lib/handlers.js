214012e15778ec10188174f19778c2ae15b19fd8
Guilhermeais <guilhermeteixeiraais@gmail.com>
2021-12-29 23:22:12 -0300

    Page 2: Create an Account

--- building-a-restful-api/app/lib/handlers.js
+++ building-a-restful-api/app/lib/handlers.js
@@ -54,6 +54,42 @@ handlers.index = function (
   }
 };
 
+// Create Account 
+handlers.accountCreate = function(data,callback){
+    // Reject any request that isn't a GET
+
+    if (data.method === "get") {
+      // Prepare data for interpolation
+      const templateData = {
+        "head.title": "Create an Account",
+        "head.description":
+          "Signup is easy and only takes a few seconds",
+        // "body.title": "Hello templated world",
+        "body.class": "accountCreate",
+      };
+  
+      // Read in a template as a string
+      helpers.getTemplate("accountCreate", templateData, (err, str) => {
+        if (!err && str) {
+          // Add the universal header and footer
+          helpers.addUniversalTemplates(str, templateData, (err, fullStr) => {
+            if (!err && fullStr) {
+              // Return that page as HTML
+              callback(200, fullStr, "html");
+            } else {
+              callback(500, undefined, "html");
+            }
+          });
+        } else {
+          callback(500, undefined, "html");
+        }
+      });
+    } else {
+      // Return that the method isn't allowed
+      callback(405, undefined, "html");
+    }
+}
+
 // Favicon
 handlers.favicon = function (data, callback) {
   // Reject any request that isn't a GET
