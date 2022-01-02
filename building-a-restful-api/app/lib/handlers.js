2622525ad64a8047fa72b22b26907267d7e31377
Guilhermeais <guilhermeteixeiraais@gmail.com>
2022-01-02 08:55:44 -0300

    Page 8: Dashboard

--- building-a-restful-api/app/lib/handlers.js
+++ building-a-restful-api/app/lib/handlers.js
@@ -261,6 +261,39 @@ handlers.checksCreate = function (data, callback) {
   }
 };
 
+// Dashboard (view all checks)
+handlers.checksList = function (data, callback) {
+  // Reject any request that isn't a GET
+
+  if (data.method === "get") {
+    // Prepare data for interpolation
+    const templateData = {
+      "head.title": "Dashboard",
+      "body.class": "checksList",
+    };
+
+    // Read in a template as a string
+    helpers.getTemplate("checksList", templateData, (err, str) => {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, fullStr) => {
+          if (!err && fullStr) {
+            // Return that page as HTML
+            callback(200, fullStr, "html");
+          } else {
+            callback(500, undefined, "html");
+          }
+        });
+      } else {
+        callback(500, undefined, "html");
+      }
+    });
+  } else {
+    // Return that the method isn't allowed
+    callback(405, undefined, "html");
+  }
+};
+
 // Favicon
 handlers.favicon = function (data, callback) {
   // Reject any request that isn't a GET
