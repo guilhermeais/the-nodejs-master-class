3659f87adbda7df401c912130386384d8d0b4041
Guilhermeais <guilhermeteixeiraais@gmail.com>
2022-01-02 08:52:33 -0300

    page 7: Create a check

--- building-a-restful-api/app/lib/handlers.js
+++ building-a-restful-api/app/lib/handlers.js
@@ -228,6 +228,39 @@ handlers.accountEdit = function (data, callback) {
   }
 };
 
+// Create a new check
+handlers.checksCreate = function (data, callback) {
+  // Reject any request that isn't a GET
+
+  if (data.method === "get") {
+    // Prepare data for interpolation
+    const templateData = {
+      "head.title": "Create a new Check",
+      "body.class": "checksCreate",
+    };
+
+    // Read in a template as a string
+    helpers.getTemplate("checksCreate", templateData, (err, str) => {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, fullStr) => {
+          if (!err && fullStr) {
+            // Return that page as HTML
+            callback(200, fullStr, "html");
+          } else {
+            callback(500, undefined, "html");
+          }
+        });
+      } else {
+        callback(500, undefined, "html");
+      }
+    });
+  } else {
+    // Return that the method isn't allowed
+    callback(405, undefined, "html");
+  }
+};
+
 // Favicon
 handlers.favicon = function (data, callback) {
   // Reject any request that isn't a GET
